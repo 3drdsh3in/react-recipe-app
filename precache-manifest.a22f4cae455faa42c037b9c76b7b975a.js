77d89d1c284458c055f932cc030bc9fc5da4a99a
Edward <edwrdshen@gmail.com>
2020-02-13 15:49:07 +1100

    Updates

---
+++ precache-manifest.a22f4cae455faa42c037b9c76b7b975a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f59411ad1863e244cfca8ad54b86fd",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "e8b6c48e9a96c47561eb",
+    "url": "/react-recipe-app/static/css/main.b2fddbd4.chunk.css"
+  },
+  {
+    "revision": "3542ece75dfdb86ed838",
+    "url": "/react-recipe-app/static/js/2.b6d8d649.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-recipe-app/static/js/2.b6d8d649.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8b6c48e9a96c47561eb",
+    "url": "/react-recipe-app/static/js/main.ec002c36.chunk.js"
+  },
+  {
+    "revision": "86717b2b445c8706b2d3",
+    "url": "/react-recipe-app/static/js/runtime-main.6d97e4b6.js"
+  }
+]);
